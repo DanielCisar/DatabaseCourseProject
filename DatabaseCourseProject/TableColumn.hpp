2160d036c5f5df12362dca6a23f3c8d2143feacf
Daniel Cisar <daniel.cisar9@gmail.com>
2025-05-17 15:28:55 +0300

    created abstract

--- DatabaseCourseProject/TableColumn.hpp
+++ DatabaseCourseProject/TableColumn.hpp
@@ -3,27 +3,10 @@
 #include <string>
 
 class TableColumn {
-private:
-	std::string name;
-	std::vector<std::string> content;
-	std::string type;
-
-	template <typename T>
-	bool checkEquivelenceBetweenColumnAndType(const T& value);
-
 public:
-	TableColumn(std::string name, std::string type);
-	~TableColumn();
+	~TableColumn() {};
 
-	std::string getName();
-	std::string getType();
-	std::vector<std::string> getContent();
-
-	template <typename T>
-	int findIndexOfGivenCell(T value);
-
-	template <typename T>
-	void changeValueAtGivenIndex(int index, T value);
-
+	virtual std::string getName() const = 0;
+	virtual std::string getType() const = 0;
 
 };
